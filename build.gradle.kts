e83b9c061ec3605e005a2368dd2de1eec816be0e
DongHyun Kim <75498839+eastshine12@users.noreply.github.com>
2024-09-30 16:41:54 +0900

    Feat: Web dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ allOpen {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.projectlombok:lombok")
