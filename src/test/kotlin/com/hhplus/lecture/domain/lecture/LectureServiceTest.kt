e59c8fa9b57ea5188a87927087fd93b9eb1c016b
eastshine <eastshine@pointi.com>
2024-10-02 18:27:36 +0900

    Test: 동일 강의에 대한 중복 수강 통합테스트 작성

--- src/test/kotlin/com/hhplus/lecture/domain/lecture/LectureServiceTest.kt
+++ src/test/kotlin/com/hhplus/lecture/domain/lecture/LectureServiceTest.kt
@@ -1,10 +1,9 @@
 package com.hhplus.lecture.domain.lecture
 
 import com.hhplus.lecture.domain.common.exception.LectureNotFoundException
+import com.hhplus.lecture.domain.common.exception.RegistrationAlreadyExistsException
 import com.hhplus.lecture.domain.user.User
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -48,6 +47,7 @@ class LectureServiceTest {
 
         every { lectureRegistrationRepository.save(any()) } returns lectureRegistration
         every { lectureRepository.findByIdOrNullWithLock(lecture.id!!) } returns lecture
+        every { lectureRegistrationRepository.findByUserIdAndLectureId(userId, lectureId) } returns null
 
         // when
         val result: LectureRegistration = lectureService.register(user, lectureId)
@@ -56,6 +56,25 @@ class LectureServiceTest {
         assertEquals(userId, result.user.id)
         assertEquals(lectureId, result.lecture.id)
         assertTrue(lecture.registeredCount == 1)
+    }
+
+    @Test
+    fun `사용자가 동일한 강의를 신청할 경우 예외가 발생해야 한다`() {
+        // given
+        val userId = 1L
+        val lectureId = 1L
+        val user = User(id = userId, name = "홍길동", email = "hong@example.com")
+        val lecture = Lecture(id = lectureId, title = "코틀린 강의", lecturerName = "강사1", date = LocalDate.now(), registeredCount = 0)
+        val lectureRegistration = LectureRegistration(user = user, lecture = lecture)
+
+        every { lectureRegistrationRepository.findByUserIdAndLectureId(userId, lectureId) } returns lectureRegistration
+
+        // when & then
+        val exception = assertThrows<RegistrationAlreadyExistsException> {
+            lectureService.register(user, lectureId)
+        }
+
+        assertEquals("이미 신청한 강의입니다.", exception.message)
     }
 
     @Test
